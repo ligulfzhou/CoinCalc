a6903df5e9ae7c2cd5d678efe8a5606077a0df0b
ligulfzhou <ligulfzhou53@gmail.com>
2018-01-25 22:35:28 +0800

    shell file

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,17 +1,31 @@
-# cd /Users/zhouligang/go/src/CoinCalc/server
-# rm server
-# 
-# env GOOS=linux GOARCH=amd64 go build -v CoinCalc/server
-# 
-# scp server aws:/home/ubuntu/source/go_coincalc/server.bk
-ssh aws << EOF
-#cd /home/ubuntu/source/go_coincalc
-#rm server
-#mv server.bk server
+upload() {
+cd /Users/zhouligang/go/src/CoinCalc/server
+rm server
 
-for i in `8801...8803`
-do
-svc restart coincalc:coincalc-$$i
-done
+env GOOS=linux GOARCH=amd64 go build -v CoinCalc/server
 
+ssh aws <<EOF
+cd /home/ubuntu/source/go_coincalc
+rm server
 EOF
+
+scp server aws:/home/ubuntu/source/go_coincalc/
+}
+
+restart() {
+ssh aws << EOF
+svc restart coincalc:coincalc-8800
+svc restart coincalc:coincalc-8801
+svc restart coincalc:coincalc-8802
+svc restart coincalc:coincalc-8803
+EOF
+}
+
+if [ $1 = "restart" ] ; then
+    echo "restart"
+    restart
+else
+    echo "deploy"
+    upload
+    restart
+fi
